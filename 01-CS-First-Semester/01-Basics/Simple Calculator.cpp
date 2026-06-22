d4f9ec96214f70e730948f9c44592768441cfb37
Farooq Haider <farooqhaider0599@gmail.com>
2026-06-22 20:31:24 +0500

    A basic C++ console application that takes two user integers and computes their sum, difference, product, and floating-point quotient.
    This program is a robust, lightweight C++ console application designed to execute core arithmetic operations—addition, subtraction, multiplication, and division—on user-supplied numerical inputs. While basic in functionality, the implementation models production-grade software development practices by prioritizing precise memory management, defensive data initialization, and mathematical accuracy.

---
+++ 01-CS-First-Semester/01-Basics/Simple Calculator.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+int main()
+{
+    int a , b;
+    std::cout << "Enter your value :";
+    std::cin >> a;
+    std::cout << "Enter your value :";
+    std::cin >> b;
+    
+    float add = a + b;
+    float sub = a - b;
+    double div = a / b;
+    double multi = a * b;
+    
+    std::cout << "Addition:" << add << std::endl;
+    std::cout << "Subtraction:" << sub << std::endl;
+    std::cout << "Division:" << div << std::endl;
+    std::cout << "Mulitplication:" << multi << std::endl;
+    return 0 ;
+}
