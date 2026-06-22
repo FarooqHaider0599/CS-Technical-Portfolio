5d054822c6c24a14f617acd8e43b5f72fd2ee393
Farooq Haider <farooqhaider0599@gmail.com>
2026-06-22 20:09:25 +0500

    Add swapping code for two integers
    This program demonstrates swapping two integers using a temporary variable.

---
+++ 01-CS-First-Semester/01-Basics/swapping_code.cpp
@@ -0,0 +1,33 @@
+//Author: M.Farooq Haider
+//Description: Demonstrates fundamental memory manipulation by swapping the values of two user-defined integers using a temporary storage variable.
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int firstNumber, secondNumber;
+
+    // User Input
+    cout << "Enter value of x: ";
+    cin >> firstNumber;
+    cout << "Enter value of y: ";
+    cin >> secondNumber;
+
+    // Display values before swapping
+    cout << "\n--- Before Swapping ---" << endl;
+    cout << "Value of x: " << firstNumber << endl;
+    cout << "Value of y: " << secondNumber << endl;
+
+    // Swapping logic using a temporary variable
+    int temp = firstNumber;
+    firstNumber = secondNumber;
+    secondNumber = temp;
+
+    // Display values after swapping
+    cout << "\n--- After Swapping ---" << endl;
+    cout << "Value of x: " << firstNumber << endl;
+    cout << "Value of y: " << secondNumber << endl;
+
+    return 0;
+}
