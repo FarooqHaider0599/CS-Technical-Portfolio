016f8e759f8d75056f63ed44849e22a7597a7d40
Farooq Haider <farooqhaider0599@gmail.com>
2026-06-23 20:12:13 +0500

    This program demonstrates signed integer overflow and underflow using a short data type in C++
    This program demonstrates signed integer overflow and underflow using the short data type in C++. It serves as a clear, educational example of how signed integers behave at their extreme limits when arithmetic operations push them beyond their representable range.

---
+++ 01-CS-First-Semester/01-Basics/Overflow+Underflow.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main()
+{
+	short a = 32767;
+	std::cout << a << std::endl;
+	a = a + 1; //this code shows Overflow as the value exceeds max value,it shows the -ve max value
+	std::cout << a << std::endl;
+	a = a - 1; //this code shows Underflow as the value exceeds max value,it shows the +ve max value
+	std::cout << a << std::endl;
+	return 0;
+}
