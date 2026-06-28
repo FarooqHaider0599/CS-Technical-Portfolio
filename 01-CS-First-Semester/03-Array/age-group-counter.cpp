800f1ca14868757bed4d770019716516bf7fb6eb
Farooq Haider <farooqhaider0599@gmail.com>
2026-06-28 13:46:25 +0500

    Update age-group-counter.cpp

--- 01-CS-First-Semester/03-Array/age-group-counter.cpp
+++ 01-CS-First-Semester/03-Array/age-group-counter.cpp
@@ -1,20 +1,19 @@
-//Name: Farooq Hiader
+//Name: M.Farooq Hiader
 //Purpose: Inputs ages of persons and counts how many are between 50 and 60
-
 #include<iostream>
 using namespace std;
 int main()
 {
 	int age[150], i, n, count = 0;
 	cout<< "Enter the number of persons required: ";
-	cin>>n;
+	cin>>n;//input;
 	cout<<"Enter ages of "<<n<<" persons."<<endl;
-	for(i=0;i<n;i++)
+	for(i=0;i<n;i++)//Checks if the age lies in between 50 && 60;
 	{
 		cin>>age[i];
 		if(age[i] >= 50 && age[i] <= 60)
 		   count = count + 1;
 	}
-	cout<< count<<" persons are between 50 and 60.";
+	cout<< count<<" persons are between 50 and 60.";//Output;
 	return 0;
 }
