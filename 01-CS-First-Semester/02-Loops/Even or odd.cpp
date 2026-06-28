d2f93d6c1d644617344ea8e7f7df772603b9a5ca
Farooq Haider <farooqhaider0599@gmail.com>
2026-06-28 13:49:42 +0500

    Add comments for author and purpose in Even or odd.cpp
    Added author and purpose comments to the program.

--- 01-CS-First-Semester/02-Loops/Even or odd.cpp
+++ 01-CS-First-Semester/02-Loops/Even or odd.cpp
@@ -1,3 +1,5 @@
+//Author: M.Farooq Haider
+//Purpose: This C++ program prompts the user to enter an integer number and determines whether it is even or odd using the modulo operator (%).
 #include<iostream>
 int main ()
 {
