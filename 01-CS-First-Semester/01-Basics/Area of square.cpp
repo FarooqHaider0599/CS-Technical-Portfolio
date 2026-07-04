5532bbff94337478e6366a47d11310ca84ce100f
Farooq Haider <farooqhaider0599@gmail.com>
2026-07-04 10:10:34 +0500

    Add comments for author and purpose of the program
    Added author and purpose comments to the program.

--- 01-CS-First-Semester/01-Basics/Area of square.cpp
+++ 01-CS-First-Semester/01-Basics/Area of square.cpp
@@ -1,7 +1,10 @@
+//Author: M.Farooq Haider
+//Purpose: This program calculates the geometric area of a shape using predefined dimensions (20 and 40) and prints the final result of 800 to the console.
+
 #include <iostream>
 using namespace std;
-
-int main() {
+int main() 
+{
     int length = 20;
     int width = 40;
     int area = length * width ;
