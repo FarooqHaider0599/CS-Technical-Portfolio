24174f61afb1ddd21bf33de5daa622aa9132e32b
Farooq Haider <farooqhaider0599@gmail.com>
2026-07-05 09:57:50 +0500

    Update Multiplication table.cpp

--- 01-CS-First-Semester/02-Loops/Multiplication table.cpp
+++ 01-CS-First-Semester/02-Loops/Multiplication table.cpp
@@ -1,15 +1,16 @@
-#include<iostream>
+//Author: M.Farooq Haider
+//Purpose: The purpose of this code is to take an integer input from the user and generate its corresponding multiplication table from one to ten.
 
+#include<iostream>
+using namespace std;
 int main()
 {
-	int number ;
-	
-	std::cout<<"Enter a Nmuber: ";
-	std::cin>>number;
-	
+	int num ;
+	cout<<"Enter a Nmuber: ";
+	cin>>num ;
 	for(int i = 1; i <=10 ; i++)
 	{
-		std::cout<<number<<" x "<<i<<" = "<<number * i<<std::endl;
+		cout<<num<<" x "<<i<<" = "<<num * i<<std::endl;
 	}
 	return 0;
 }
