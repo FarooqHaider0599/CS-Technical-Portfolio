3f7a9e45d87ad4be0bc651bbe6a672ae8b87a590
Farooq Haider <farooqhaider0599@gmail.com>
2026-07-08 11:10:53 +0500

    Fix formatting and update comments in Even_or_odd.cpp

--- 01-CS-First-Semester/02-Loops/Even_or_odd.cpp
+++ 01-CS-First-Semester/02-Loops/Even_or_odd.cpp
@@ -1,20 +1,20 @@
 //Author: M.Farooq Haider
 //Purpose: This C++ program prompts the user to enter an integer number and determines whether it is even or odd using the modulo operator (%).
+
 #include<iostream>
 int main ()
 {
 	int number;
-	
 	std::cout<<"Enter a number: ";
 	std::cin>>number;
 	
 	if (number % 2 == 0)
     {
-    std::cout<<"The number is even. ";
+      std::cout<<"The number is even. ";
     }   
     else
     {
-    std::cout<<"The number is odd. ";
+      std::cout<<"The number is odd. ";
 	}
 	return 0 ;
 }
