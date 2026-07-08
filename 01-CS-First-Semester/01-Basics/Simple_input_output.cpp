54c28e95e84ded48469be87927d182e2994ab795
Farooq Haider <farooqhaider0599@gmail.com>
2026-07-08 11:09:22 +0500

    Add comments for author and purpose in Simple_input_output.cpp
    Added author and purpose comments to the code.

--- 01-CS-First-Semester/01-Basics/Simple_input_output.cpp
+++ 01-CS-First-Semester/01-Basics/Simple_input_output.cpp
@@ -1,12 +1,16 @@
+//Author:M.Farooq Haider
+//Purpose:This code captures a user's name and age via standard input and prints a personalized greeting displaying both pieces of information.
+
 #include<iostream>
 int main ()
 {
-	std::string name;
+	string name;
 	int age;
 	std::cout<<"Enter your name:";
 	std::cin>>name;
 	std::cout<<"Enter your age:";
 	std::cin>>age;
+	
 	std::cout<<"\nHello " << name << " You are " << age << " Years old"<<std::endl;
 	return 0;
 }
