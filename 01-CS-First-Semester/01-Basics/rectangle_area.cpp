1eedfa37ccf47424ae11ef39590eeb388102763a
Farooq Haider <farooqhaider0599@gmail.com>
2026-07-09 18:27:41 +0500

    Refactor to rectangle_area.cpp with updated comments
    Updated comments and file name to reflect rectangle area calculation.

---
+++ 01-CS-First-Semester/01-Basics/rectangle_area.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file     rectangle_area.cpp
+ * @author   Muhammad Farooq Haider
+ * @brief    Calculates and displays the geometric area of a rectangle 
+ * using predefined hardcoded dimensions.
+ *
+ * Course:   Introduction to Programming (CS101)
+ * Semester: Spring 2026
+ */
+
+#include <iostream>
+int main() 
+{
+    int length = 20;
+    int width = 40;
+    
+    int area = length * width ;
+    
+    std::cout << "Area of square:"<< area ;
+    return 0;
+}
