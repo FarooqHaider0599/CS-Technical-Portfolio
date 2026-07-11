8255d4fab50a845856bb462e89b8e4975af55ec1
Farooq Haider <farooqhaider0599@gmail.com>
2026-07-11 17:10:53 +0500

    Update Multiplication table.cpp

--- 01-CS-First-Semester/01-Basics/Multiplication table.cpp
+++ 01-CS-First-Semester/01-Basics/Multiplication table.cpp
@@ -1,15 +1,25 @@
-#include<iostream>
+/**
+ * @file multiplication_table.cpp
+ * @brief A basic C++ console application that generates a multiplication table.
+ * @details This program prompts the user for a single integer and utilizes a
+ *          for-loop to calculate and print its multiplication table from 1 to 10
+ *          using the standard output stream.
+ * 
+ * @author M.Farooq Haider
+ * @date 2026-07-11
+ */
 
+#include <iostream>
 int main()
 {
-	int number ;
-	
-	std::cout<<"Enter a Nmuber: ";
-	std::cin>>number;
-	
-	for(int i = 1; i <=10 ; i++)
-	{
-		std::cout<<number<<" x "<<i<<" = "<<number * i<<std::endl;
-	}
-	return 0;
+    int number;
+    
+    std::cout << "Enter a Number: "; // Fixed the typo here as well!
+    std::cin >> number;
+    
+    for(int i = 1; i <= 10; i++)
+    {
+        std::cout << number << " x " << i << " = " << number * i << std::endl;
+    }
+    return 0;
 }
