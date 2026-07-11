3528d54c7dd3891ed971590fb06946ed6b114df5
Farooq Haider <farooqhaider0599@gmail.com>
2026-07-11 17:19:44 +0500

    Revise comments and author information in multiplication_table.cpp
    Updated author name and documentation details.

--- 01-CS-First-Semester/01-Basics/multiplication_table.cpp
+++ 01-CS-First-Semester/01-Basics/multiplication_table.cpp
@@ -1,20 +1,19 @@
 /**
- * @file multiplication_table.cpp
- * @brief A basic C++ console application that generates a multiplication table.
- * @details This program prompts the user for a single integer and utilizes a
- *          for-loop to calculate and print its multiplication table from 1 to 10
- *          using the standard output stream.
- * 
- * @author M.Farooq Haider
- * @date 2026-07-11
+ * @file      multiplication_table.cpp
+ * @author    Muhammad Farooq Haider
+ * @brief     Takes an integer input and displays its multiplication table from 1 to 10.
+ *
+ * Course:    Introduction to Programming (CS101)
+ * Semester:  Spring 2026
  */
 
 #include <iostream>
+
 int main()
 {
     int number;
     
-    std::cout << "Enter a Number: "; // Fixed the typo here as well!
+    std::cout << "Enter a Number: "; 
     std::cin >> number;
     
     for(int i = 1; i <= 10; i++)
